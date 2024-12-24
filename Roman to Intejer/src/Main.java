0c946e6e2f7143459fb69ead22ba4462b85c6558
CoderQudratov <coderqudratov@gmail.com>
2024-12-24 20:35:21 +0500

    add loop add condition

--- Roman to Intejer/src/Main.java
+++ Roman to Intejer/src/Main.java
@@ -3,9 +3,26 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(romanTo);
+        System.out.println(romanToInt("XV"));
     }
-    public  int romanToInt(String str){
+    public static int romanToInt(String str){
+        Map<Character,Integer> m =new HashMap<>();
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+        int ans =0;
+        for (int i = 0; i < str.length(); i++) {
+            if (i<str.length()-1 && m.get(str.charAt(i)) <m.get(str.charAt(i+1))){
+                ans-=m.get(str.charAt(i));
 
+            }else{
+                ans+=m.get(str.charAt(i));
+            }
+        }
+        return  ans;
     }
 }
